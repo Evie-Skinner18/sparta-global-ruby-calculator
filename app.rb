f2c1684329d718dd279ed198979300f6fe8fca42
Tech-A35 <tech-a35@Tech-A35s-MacBook-Air.local>
2018-05-08 15:46:13 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,8 @@
+print "Which calculator would you like to use? Type B for basic or A for advanced"
+which_calc = gets.chomp
+if which_calc == "B"
+  puts "What operation would you like to perform? Choose (a)dd, (s)ubstract, (m)ultiply or (d)ivide."
+
+else
+
+end
